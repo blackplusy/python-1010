7c0e03a52c62ed2640fe243747b9329141a2ad4a
blackplusy <happyming07@sina.com>
2018-10-15 11:15:40 +0800

    Add files via upload

---
+++ 例子-1015.03.算数运算符.py
@@ -0,0 +1,15 @@
+#coding=utf-8
+print(10+3)
+
+print(10-3)
+
+print(10*3)
+
+print(10/3)   #3.33
+
+print(10%3)   #1
+
+print(10**3)  #1000
+
+print(10//3)  #3
+
---
+++ 例子-1015.04.逻辑运算符.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+print(1>2 and 2<3)
+#假 and 真==假
+
+print(1>0 or 2<0)
+#真 or 假==真
+
+
+print(not(1<0))
+#非假==真
+
