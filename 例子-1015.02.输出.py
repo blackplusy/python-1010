ce44d8a6fc6697b6ab832244d37158c0d75fc1c8
blackplusy <happyming07@sina.com>
2018-10-15 10:16:13 +0800

    Add files via upload

---
+++ 例子-1015.02.输出.py
@@ -0,0 +1,33 @@
+#coding=utf-8
+#直接输出，通过print()进行打印
+#打印翻滚吧牛宝宝
+print('翻滚吧！牛宝宝！！！')
+
+#变量输出
+name='python'
+print(name)
+#name=input('请输入您的年龄：')
+#print(name)
+
+#变量操作后输出
+a=20
+b=30
+print(a+b)
+c='heygor'
+d='handsome!!!!'
+print(c+d)
+
+#函数输出
+print(abs(-3))
+
+#格式化输出
+# %s   格式化字符串
+# %d   格式化整型(数字)
+name=input('请输入您的法号：')
+age=int(input('请输入您的年龄：'))
+print('您的法号是：%s,您的年龄是%d'%(name,age))
+
+#数据类型转换  int() 转换为整型  str() 转换为字符串
+
+
+
