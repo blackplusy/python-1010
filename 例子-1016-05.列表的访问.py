62d8cea4efadd05a8d92efc413d57a0cc4c66aba
blackplusy <happyming07@sina.com>
2018-10-16 14:37:50 +0800

    Add files via upload

---
+++ 例子-1016-05.列表的访问.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+li=[1,2,3,4]
+
+print(li)
+
+li2=['heygor','ladeng','8ma','langpu']
+for i in li2:
+	print(i)
+
+if  'heygor' in li2:
+	print('im here')
+
---
+++ 例子-1016-06.列表的索引和切片.py
@@ -0,0 +1,8 @@
+l=[1,2,3]
+print(l[0])
+print(l[-2])
+#print(l[3])
+
+print(l[:-1])
+print(l[1:])
+print(l[1:2])
