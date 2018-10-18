0c5fed7689c79854b7dfc488bb72e79c65a10697
blackplusy <happyming07@sina.com>
2018-10-18 09:44:36 +0800

    Add files via upload

---
+++ 例子-1018.01.函数的类型.py
@@ -0,0 +1,26 @@
+#coding=utf-8
+
+def print_hello():
+	print('hello world!!!')
+
+print_hello()
+
+def sleep():
+	return 'sleep'
+s=sleep()
+print(s)
+
+def sub(x,y):
+	print('x+y=',x+y)
+
+sub(2,3)
+
+
+def sub(x,y):
+	return x+y
+res=sub(20,30)
+print(res)
+
+
+
+
---
+++ 例子-1018.02.函数的返回值.py
@@ -0,0 +1,18 @@
+#coding=utf-8
+'''
+def sum(a,b):
+	jisuan=a+b
+	return jisuan
+s=sum(20,30)
+print(s)
+'''
+def re(a,b):
+	a*=10
+	b*=10
+	return a,b
+num=re(4,5)
+print(num)
+print(type(num))
+
+num2,num1=re(10,50)
+print(num1,num2)
