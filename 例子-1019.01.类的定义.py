bff9935a44d6975d72e8b664334d15ab9b329266
blackplusy <happyming07@sina.com>
2018-10-19 10:34:06 +0800

    Add files via upload

---
+++ 例子-1019.01.类的定义.py
@@ -0,0 +1,22 @@
+#coding=utf-8
+class student:			#创建一个类
+	def study(self):	#方法列表
+		print('im study')
+	def play(self):
+		print('play ball')
+boy=student()
+#产生一个student对象，通过boy实例对象来访问属性和方法
+boy.age=20
+#给对象添加属性，注意：后面再次出现，表示对属性进行修改
+boy.favor='football'
+#给对象添加属性
+
+boy.study()
+#通过实例对象访问类的方法
+boy.play()
+
+print(boy.age)
+print(boy.favor)
+
+
+	
